4d4baac94386ee6f88f679db7956dfbb0f3d5723
yuhw <593919790@qq.com>
2022-09-19 21:40:47 +0800

    first

--- train.py
+++ train.py
@@ -3,6 +3,10 @@ import setting
 import utils
 import module.optimizer as optimizer
 
+#  1、train文件中需要将每一个功能都分开在不同的模块文件中   2、需要修改数据集，总共有三种数据 两个标签  3、需要设置训>练集、验证集、测试集。
+
+
+
 def load_data():
     raise NotImplementedError
 
