608012955e2aaf35cf55e899fffaa61185064a50
chlience <chlience@outlook.com>
2022-10-01 14:13:11 +0800

    update data pre process

--- localtools/data_generate_split.py
+++ localtools/data_generate_split.py
@@ -28,69 +28,64 @@ dataset_pre_pkl = read_pkl(dataset_pre_pkl_path)
 faultType = dataset_pre_pkl.keys()
 
 for type1 in faultType:
-    # 1.路径
-    pos_source_path = '../data/' + type1 + '/src_token_positive.txt'
-    pos_target_path = '../data/' + type1 + '/src_token_positive_patch.txt'
-    neg_source_path = '../data/' + type1 + '/src_token_negative.txt'
-    neg_target_path = '../data/' + type1 + '/src_token_negative_patch.txt'
+    # # 1.路径
+    # pos_source_path = '../data/' + type1 + '/src_token_positive.txt'
+    # pos_target_path = '../data/' + type1 + '/src_token_positive_patch.txt'
+    # neg_source_path = '../data/' + type1 + '/src_token_negative.txt'
+    # neg_target_path = '../data/' + type1 + '/src_token_negative_patch.txt'
 
-    # 2.读取txt文件转成list
-    pos_source = []
-    pos_target = []
-    neg_source = []
-    neg_target = []
+    # # 2.读取txt文件转成list
+    # pos_source = []
+    # pos_target = []
+    # neg_source = []
+    # neg_target = []
 
-    with open(pos_source_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            line = line.split()
-            pos_source.append(line)
-        f.close()
+    # with open(pos_source_path, 'r', encoding='utf-8') as f:
+    #     for line in f:
+    #         line = line.split()
+    #         pos_source.append(line)
+    #     f.close()
 
-    with open(pos_target_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            line = line.split()
-            pos_target.append(line)
-        f.close()
+    # with open(pos_target_path, 'r', encoding='utf-8') as f:
+    #     for line in f:
+    #         line = line.split()
+    #         pos_target.append(line)
+    #     f.close()
 
-    with open(neg_source_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            line = line.split()
-            neg_source.append(line)
-        f.close()
+    # with open(neg_source_path, 'r', encoding='utf-8') as f:
+    #     for line in f:
+    #         line = line.split()
+    #         neg_source.append(line)
+    #     f.close()
 
-    with open(neg_target_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            line = line.split()
-            neg_target.append(line)
-        f.close()
+    # with open(neg_target_path, 'r', encoding='utf-8') as f:
+    #     for line in f:
+    #         line = line.split()
+    #         neg_target.append(line)
+    #     f.close()
 
     # print(len(pos_source), len(pos_target), len(neg_source), len(neg_target))
-
-    source = pos_source + neg_source
-    target = pos_target + neg_target
+    source = dataset_pre_pkl[type1]['positive'] + dataset_pre_pkl[type1]['negative']
+    target = dataset_pre_pkl[type1]['positive_patch'] + dataset_pre_pkl[type1]['negative_patch']
 
     label = []
-    for i in range(len(pos_source)):  # 地址自动变成 0 -> len(pos_source)-1
-        label.append(0)  # 定义  label[0:len(pos_source)-1]=0  指有错误的数据
-    # print("制作pos标签后lable的长度：",len(label))
+    for i in range(len(dataset_pre_pkl[type1]['positive'])):  # 地址自动变成 0 -> len(pos_source)-1
+        label.append(1)  # 定义  label[0:len(pos_source)-1]=1  指有错误的数据
 
-    for j in range(len(neg_source)):
-        # 定义  label[len(neg_source):len(neg_source)+len(pos_source)-1]=1 指没有错误的数据
-        label.append(1)
-
-    # print(len(source), len(target), len(label))
+    for j in range(len(dataset_pre_pkl[type1]['negative'])):
+        label.append(0) # 定义  label[len(neg_source):len(neg_source)+len(pos_source)-1]=1 指没有错误的数据
 
     # 1.按照地址划分数据
     index = [i for i in range(len(source))]
-    random.shuffle(index)
+    # random.shuffle(index)
     # print(type(index), index)
 
     train_scales = 0.8
     val_scales = 0.1
     test_scales = 0.1
 
-    train_stop_flage = int(len(source) * train_scales)
-    val_stop_flage = int(len(source) * (train_scales + val_scales))
+    train_stop_flag = int(len(source) * train_scales)
+    val_stop_flag = int(len(source) * (train_scales + val_scales))
 
     # 2.按照地址 存数据到pkl中
     train_data = {'source': [], 'target': [], 'label': []}
@@ -98,21 +93,25 @@ for type1 in faultType:
     test_data = {'source': [], 'target': [], 'label': []}
 
     for i, j in enumerate(index):
-        if (i <= train_stop_flage):
+        if (i <= train_stop_flag):
             train_data['source'].append(source[j])
             train_data['target'].append(target[j])
             train_data['label'].append(label[j])
                 
-        if (i > train_stop_flage and i <= val_stop_flage):
+        if (i > train_stop_flag and i <= val_stop_flag):
             valid_data['source'].append(source[j])
             valid_data['target'].append(target[j])
             valid_data['label'].append(label[j])
-        if (i > val_stop_flage):
+        if (i > val_stop_flag):
             test_data['source'].append(source[j])
             test_data['target'].append(target[j])
             test_data['label'].append(label[j])
     # 4.保存文件
     path = '../data/' + type1
+    try:
+        os.mkdir(path)
+    except Exception as e:
+        print(e)
     write_pkl(train_data, path + '/train.pkl')
     write_pkl(valid_data, path + '/valid.pkl')
     write_pkl(test_data, path + '/test.pkl')
--- localtools/raw_data_process.py
+++ localtools/raw_data_process.py
@@ -12,13 +12,12 @@ import os
                   
     ../data/dataset_pre.pkl 文件是，处理 negative标记、词长>200的 最终数据。
     
-    数据集为： 【pos,neg,pat】
-      变成：【pos(有标记),patch】==> label=1
+    数据集为：【pos,neg,pat】
+        变成：【pos(有标记),patch】==> label=1
            【neg(有标记),neg(没有标记)】 ==> label=0
     
     
 """
-
 
 def read_pkl(filepath):
     with open(filepath, "rb") as f:
@@ -29,59 +28,71 @@ def write_pkl(obj, filepath):
     with open(filepath, "wb") as f:
         pickle.dump(obj, f)
 
-
+print("### Reading pkl")
 dataset_pkl_path = '../data/dataset.pkl'
 dataset_pkl = read_pkl(dataset_pkl_path)
 
 # 一、获得token小于200词的数据集。
 #     1.将negative中的rank
-#     2.原始数据集不变，删去有效字符长度超过200的数据，以patch中的行数为准
-faultType = dataset_pkl.keys()
-dict_data = {}
-for type1 in faultType:
+#     2.原始数据集不变，删去有效字符长度超过200的数据
+# faultType = dataset_pkl.keys()
+# dict_data = {}
+# for type1 in faultType:
 
-    data_len1 = len(dataset_pkl[type1]['positive'])
-    data_len2 = len(dataset_pkl[type1]['negative'])
-    data_len3 = len(dataset_pkl[type1]['patch'])
-    print(type1, "处理前代码片段总量为：", data_len1, data_len2, data_len3, data_len2)
+#     data_len1 = len(dataset_pkl[type1]['positive'])
+#     data_len2 = len(dataset_pkl[type1]['negative'])
+#     data_len3 = len(dataset_pkl[type1]['patch'])
+#     data_len4 = len(dataset_pkl[type1]['negative'])
+#     print(type1, "处理前代码片段总量为：", data_len1, data_len2, data_len3, data_len4)
 
-    code_list_pos = []
-    code_list_neg = []
-    code_list_pos_pat = []
-    code_list_neg_pat = []
+#     code_list_pos = []
+#     code_list_neg = []
+#     code_list_pos_pat = []
+#     code_list_neg_pat = []
 
-    for i in range(data_len1):
-        # 1. 将 negative中的标记都替换掉
-        data_neg = dataset_pkl[type1]["negative"][i]
-        data_neg_patch = (dataset_pkl[type1]["negative"][i]).replace(
-            "rank2fixstart", " ").replace("rank2fixend", " ")
-        # 2.  再用javalang分词，排除大于200词的 (由于三个文件数据长度不同，为了保持数据对应维度相同，以patch为准，删去数据)
-        tokens = javalang.tokenizer.tokenize(dataset_pkl[type1]["patch"][i])
-        token_seq = []
-        for token in tokens:
-            token_seq.append(str(token.value))
-        # print(len(token_seq))
-        if(len(token_seq) <= 200):
-            # 原始有错误标记的positive
-            code_list_pos.append(dataset_pkl[type1]["positive"][i])
-            code_list_neg.append(data_neg)  # 原始有错误 fl的 negative
-            code_list_pos_pat.append(
-                dataset_pkl[type1]["patch"][i])  # 修复后的patch
-            code_list_neg_pat.append(data_neg_patch)  # 没有标记 错误fl的 negative
-    print(type1, "处理后代码片段总量为：", len(code_list_pos),
-          len(code_list_neg), len(code_list_pos_pat), len(code_list_neg_pat))
+#     for i in range(data_len1):
+#         # 1. 将 negative中的标记都替换掉,构建 neg_patch
+#         data_pos = dataset_pkl[type1]["positive"][i]
+#         data_pos_patch = dataset_pkl[type1]["patch"][i]
+#         data_neg = dataset_pkl[type1]["negative"][i]
+#         data_neg_patch = dataset_pkl[type1]["negative"][i] \
+#             .replace("rank2fixstart", " ").replace("rank2fixend", " ")
 
-    #  以上得到了具体数据 code_list,接下来组装成pkl即可。
-    dict_data[type1] = {'positive': code_list_pos, 'negative': code_list_neg,
-                        'positive_patch': code_list_pos_pat, 'negative_patch': code_list_neg_pat}
+#         # 2.  再用javalang分词，排除大于200词的 (由于三个文件数据长度不同，为了保持数据对应维度相同，
+#         #        以pos,pos_patch,neg,neg_patch同时为准，四者的长度都小于200时，才加入到新的数据中)
+#         tokens_pos = [token.value for token in javalang.tokenizer.tokenize(data_pos)]
+#         if len(tokens_pos) > 200:
+#             continue
+#         tokens_pos_patch = [token.value for token in javalang.tokenizer.tokenize(data_pos_patch)]
+#         if len(tokens_pos_patch) > 200:
+#             continue
+#         tokens_neg = [token.value for token in javalang.tokenizer.tokenize(data_neg)]
+#         if len(tokens_neg) > 200:
+#             continue
+#         tokens_neg_patch = [token.value for token in javalang.tokenizer.tokenize(data_neg_patch)]
+#         if len(tokens_neg_patch) > 200:
+#             continue
+    
+#         code_list_pos.append(tokens_pos)  # 原始有错误标记的positive
+#         code_list_neg.append(tokens_neg)  # 原始有错误 fl的 negative
+#         code_list_pos_pat.append(tokens_pos_patch)  # 修复后的patch
+#         code_list_neg_pat.append(tokens_neg_patch)  # 没有标记 错误fl的 negative
+    
+#     print(type1, "处理后代码片段总量为：", len(code_list_pos),
+#           len(code_list_neg), len(code_list_pos_pat), len(code_list_neg_pat))
 
-print(dict_data.keys())
-write_pkl(dict_data, "../data/dataset_pre.pkl")
+#     #  以上得到了具体数据 code_list,接下来组装成pkl即可。
+#     dict_data[type1] = {'positive': code_list_pos, 'negative': code_list_neg,
+#                         'positive_patch': code_list_pos_pat, 'negative_patch': code_list_neg_pat}
+
+# print(dict_data.keys())
+# write_pkl(dict_data,"../data/dataset_pre.pkl" )
 
 
 # 二、制作数据样本，生成 11种错误类型的 11 个文件夹，每个文件夹有三个txt文件，代表 'positive', 'negative', 'patch'
 #     数据的形式为 list[str,str,...,str]
 
+print("### Reading pkl")
 dataset_pre_pkl_path = '../data/dataset_pre.pkl'
 dataset_pre_pkl = read_pkl(dataset_pre_pkl_path)
 
@@ -90,28 +101,14 @@ codeType = ['positive', 'negative', 'positive_patch', 'negative_patch']
 
 for type1 in faultType:
     for type1_1 in codeType:
+        print(type1 + " " + type1_1)
         list1 = dataset_pre_pkl[type1][type1_1]
-        print("处理前 "+type1 + '的' + type1_1 + '的长度为：', len(list1))
+        print(str(type(list1)))
         file_path = '../data/' + type1
         if not os.path.isdir(file_path):
             os.makedirs(file_path)
-        file = open(file_path + '/src_token_' + type1_1 +
-                    '.txt', 'w', encoding='utf-8')
-
-        for i in range(len(list1)):
-            tokens1 = javalang.tokenizer.tokenize(list1[i])
-            tokens1_seq = []
-            for token1 in tokens1:
-                tokens1_seq.append(str(token1.value))
-            # print(tokens1_seq)  # 得到的tokens1_seq样式为：['private', 'void', 'handleAuthorityURL', '(', 'List', '<',...],之后用.join()方法拼接
-            string11 = ' '.join(tokens1_seq)
-            # 如果遇到代码中包含非法字符时，用？？替代
-            string111 = string11.encode('utf-8', 'replace').decode('utf-8')
-
-            file.write(string111)
-            file.write('\n')
-        file.close()
-
-        count = len(open(file_path + '/src_token_' +
-                    type1_1 + '.txt', 'r').readlines())
-        print("处理后" + type1 + '的' + type1_1 + "文件的长度度为：", count)
+        with open(file_path + '/token_' + type1_1 + '.txt', 'w', encoding='utf-8') as file:
+            for line in list1:
+                string = ' '.join(line).encode('utf-8', 'replace').decode('utf-8')
+                file.write(string)
+                file.write('\n')
